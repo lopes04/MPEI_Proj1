a57465b6ccb0c21f7ec66a09560ec80876eee051
Henrique Lopes <henriquelopes@ua.pt>
2024-11-26 15:06:25 +0000

    comecei o tratamento do texto

--- main.m
+++ main.m
@@ -1,3 +1,29 @@
 % Ler o ficheiro CSV
-dados = readtable('dataset112.csv');
-disp(dados);
+frases = readtable('dataset112.csv');
+
+% excluir duplicadas
+frases = unique(frases, 'rows');
+
+%meter texto em minusculas
+frases = lower(frases);
+disp(frases)
+
+%------------------------------
+
+% Acessar apenas a coluna de texto para normalização
+textData = frases.Text;
+
+% Converter texto para minúsculas
+textData = lower(textData);
+
+% Remover pontuações
+%textData = erasePunctuation(textData);
+
+% Remover números e caracteres especiais
+textData = regexprep(textData, '[^a-zA-Z\s]', ''); % Remove tudo exceto letras e espaços
+
+% Substituir a coluna normalizada na tabela original (se necessário)
+frases.Text = textData;
+
+% Exibir as frases normalizadas
+disp(frases);
